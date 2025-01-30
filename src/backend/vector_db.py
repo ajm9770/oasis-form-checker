09af24078e44d01e9305052671fb7dafe4772303
Avi Mahajan <mahajan.avi@gmail.com>
2025-01-30 15:06:28 -0500

    Integrate HuggingFace embeddings and Chroma for vector database setup

--- src/backend/vector_db.py
+++ src/backend/vector_db.py
@@ -1,10 +1,22 @@
 # Using ChromaDB
-import chromadb
-from chromadb.config import Settings
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+import os
+import getpass
 
-client = chromadb.Client(Settings(
-    chroma_db_impl="duckdb+parquet",
-    persist_directory=".chroma_db"
-))
+if not os.environ.get("HUGGINGFACEHUB_API_TOKEN"):
+  os.environ["HUGGINGFACEHUB_API_TOKEN"] = getpass.getpass("Enter API key for HuggingFace: ")
 
-collection = client.create_collection("patient_docs")
+embeddings = HuggingFaceEmbeddings(
+    model_name="HuggingFaceH4/zephyr-7b-beta",
+    model_kwargs={
+      "device": "cpu",
+    },
+    encode_kwargs={'normalize_embeddings': False}
+)
+
+vector_store = Chroma(
+    collection_name="patient_ids",
+    embedding_function=embeddings,
+    persist_directory="../../database/chroma_langchain_test"  # Where to save data locally, remove if not necessary
+)
